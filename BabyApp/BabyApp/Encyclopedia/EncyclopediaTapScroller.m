aa4cc60a5071dcafe589522174698add66c7b646
atulawasthimarch <atul.iosdeveloper@gmail.com>
2016-04-04 09:09:57 +0530

    Commit With Scroller issue Solved under Encyclopaedia Tab

--- BabyApp/BabyApp/Encyclopedia/EncyclopediaTapScroller.m
+++ BabyApp/BabyApp/Encyclopedia/EncyclopediaTapScroller.m
@@ -17,7 +17,8 @@
 @implementation EncyclopediaTapScroller
 @synthesize scrollerTable,scrollerTable2,scroll1,page1;
 NSArray *labelArrayScroller,*labelArrayScroller2;
--(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+/*
+ -(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
     CGFloat width = scrollView.frame.size.width;
     NSInteger page = (scrollView.contentOffset.x + (0.5f * width)) / width;
@@ -26,16 +27,13 @@ NSArray *labelArrayScroller,*labelArrayScroller2;
     self.navigationItem.title = [hed objectAtIndex:page];
     
     
-    /*if((((int)scrollView.contentOffset.x)%((int)scrollView.frame.size.width))<=0)
+    if((((int)scrollView.contentOffset.x)%((int)scrollView.frame.size.width))<=0)
      [page1 setCurrentPage:0];
      else
-     [page1 setCurrentPage:1];*/
+     [page1 setCurrentPage:1];
     
     NSLog(@"current page=%ld",(long)page1.currentPage);
-    
-    [scrollerTable reloadData];
-}
-
+}*/
 NSArray *hed;
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -44,24 +42,109 @@ NSArray *hed;
     
     
     
-    /*
-     
-     
-     scrollerTable=[[UITableView alloc] initWithFrame:CGRectMake(0, v.frame.origin.y+v.frame.size.height+10, self.view.frame.size.width, self.view.frame.size.height-(v.frame.origin.y+v.frame.size.height+20))];
-     [self.view addSubview:scrollerTable];
-     scrollerTable.dataSource=self;
-     scrollerTable.delegate=self;
-     
-     scrollerTable2=[[UITableView alloc] initWithFrame:CGRectMake(0, v.frame.origin.y+v.frame.size.height+10, self.view.frame.size.width, self.view.frame.size.height-(v.frame.origin.y+v.frame.size.height+20))];
-     [self.view addSubview:scrollerTable2];
-     scrollerTable2.dataSource=self;
-     scrollerTable2.delegate=self;
-     */
+    labelArrayScroller=[NSArray arrayWithObjects:@"ANTIHISTAMINES",@"FEVER MEDICATIONS",@"COUGH EXPETORANTS",@"MUCOLYTICS",@"MIXED COUGH PREPARATIONS",@"NASAL",@"LOZENGES",@"ANTINIOTICS",@"ANTIEMETICS",@"ANTISPAMODIC",@"TOPICAL", nil];
+    
+    labelArrayScroller2=[NSArray arrayWithObjects:@"BCG",@"HEPATITIS B",@"DTAP",@"MMR", nil];
+    
+    CGRect scrollFrame = CGRectMake(0, 60, [self.view bounds].size.width, [self.view bounds].size.height-60);
+    scroll1 = [[UIScrollView alloc]initWithFrame: scrollFrame];
+    
+    [self.view addSubview:scroll1];
+    [scroll1 setBackgroundColor:[UIColor blueColor]];
+    
+    scroll1.delegate=self;
+    
+    UIView *v1=[[UIView alloc] initWithFrame:CGRectMake(0, 0, scroll1.frame.size.width, scroll1.frame.size.height)];
+    [scroll1 addSubview:v1];
+    [v1 setBackgroundColor:[UIColor redColor]];
+    
+    UIView *v2=[[UIView alloc] initWithFrame:CGRectMake(scroll1.frame.size.width, 0, scroll1.frame.size.width, scroll1.frame.size.height)];
+    
+    NSLog(@"self.view height=%f scrollbar height=%f v1 hight=%f",self.view.frame.size.height,scroll1.frame.size.height,v1.frame.size.height);
+    [scroll1 addSubview:v2];
+    [scroll1 setContentSize:CGSizeMake(scroll1.frame.size.width*2, scroll1.frame.size.height)];
+    [v1 setBackgroundColor:[UIColor redColor]];
+    [v2 setBackgroundColor:[UIColor greenColor]];
+    
+    [scroll1 setBounces:NO];
+    
+    [scroll1 setPagingEnabled:YES];
+    UIView *v=[[UIView alloc] initWithFrame:CGRectMake(0, 0,scroll1.frame.size.width, 150)];
+    [v setBackgroundColor:[UIColor colorWithRed:49.0/255.0 green:191.0/255.0 blue:180.0/255.0 alpha:1.0]];
+    [v1 addSubview:v];
+    
+    UILabel *lbl1=[[UILabel alloc] initWithFrame:CGRectMake(10,5, self.view.frame.size.width-20, 40)];
+    [v addSubview:lbl1];
+    
+    UISearchBar *search=[[UISearchBar alloc] initWithFrame:CGRectMake(10,55, self.view.frame.size.width-20, 40)];
+    [v addSubview:search];
+    
+    UIView *vLine=[[UIView alloc] initWithFrame:CGRectMake(0, 105, self.view.frame.size.width,2)];
+    [vLine setBackgroundColor:[UIColor whiteColor]];
+    [v addSubview:vLine];
+    
+    UILabel *lbl2=[[UILabel alloc] initWithFrame:CGRectMake(10,117, self.view.frame.size.width-20, 40)];
+    [v addSubview:lbl2];
+    
+    [lbl1 setText:@"Type in for a quick search in the database"];
+    [lbl2 setText:@"SEARCH BY CATEGORIES BELOW"];
+    
+    [lbl1 setTextAlignment:NSTextAlignmentCenter];
+    [lbl2 setTextAlignment:NSTextAlignmentCenter];
+    
+    [lbl1 setTextColor:[UIColor whiteColor]];
+    [lbl2 setTextColor:[UIColor whiteColor]];
+    
+    UIView *vv2=[[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 150)];
+    [vv2 setBackgroundColor:[UIColor colorWithRed:49.0/255.0 green:191.0/255.0 blue:180.0/255.0 alpha:1.0]];
+    [v2 addSubview:vv2];
+    
+    UILabel *lbl12=[[UILabel alloc] initWithFrame:CGRectMake(10,5, self.view.frame.size.width-20, 40)];
+    [vv2 addSubview:lbl12];
+    
+    UISearchBar *search2=[[UISearchBar alloc] initWithFrame:CGRectMake(10,55, self.view.frame.size.width-20, 40)];
+    [vv2 addSubview:search2];
+    
+    UIView *vLine2=[[UIView alloc] initWithFrame:CGRectMake(0, 105, self.view.frame.size.width,2)];
+    [vLine2 setBackgroundColor:[UIColor whiteColor]];
+    [vv2 addSubview:vLine2];
+    
+    UILabel *lbl22=[[UILabel alloc] initWithFrame:CGRectMake(10,117, self.view.frame.size.width-20, 40)];
+    [vv2 addSubview:lbl22];
+    
+    [lbl12 setText:@"Type in for a quick search in the database"];
+    [lbl22 setText:@"SEARCH BY CATEGORIES BELOW"];
+    
+    [lbl12 setTextAlignment:NSTextAlignmentCenter];
+    [lbl22 setTextAlignment:NSTextAlignmentCenter];
+    
+    [lbl12 setTextColor:[UIColor whiteColor]];
+    [lbl22 setTextColor:[UIColor whiteColor]];
+    
+    
+    scrollerTable=[[UITableView alloc] initWithFrame:CGRectMake(0, v.frame.origin.y+v.frame.size.height, self.view.frame.size.width, self.view.frame.size.height-(v.frame.origin.y+v.frame.size.height))];
+    [v1 addSubview:scrollerTable];
+    scrollerTable.dataSource=self;
+    scrollerTable.delegate=self;
+    
+    scrollerTable2=[[UITableView alloc] initWithFrame:CGRectMake(0, vv2.frame.origin.y+vv2.frame.size.height, self.view.frame.size.width, scroll1.frame.size.height-(vv2.frame.origin.y+vv2.frame.size.height))];
+    [v2 addSubview:scrollerTable2];
+    scrollerTable2.dataSource=self;
+    scrollerTable2.delegate=self;
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    page1=[[UIPageControl alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2-15, 60, 30, 20)];
+    [page1 setNumberOfPages:2];
+    [page1 setCurrentPage:1];
+    
+    [self.view addSubview:page1];
+    [self.view bringSubviewToFront:page1];
     
 }
 
 
--(void)viewWillAppear:(BOOL)animated
+/*-(void)viewWillAppear:(BOOL)animated
 {
     
     labelArrayScroller=[NSArray arrayWithObjects:@"ANTIHISTAMINES",@"FEVER MEDICATIONS",@"COUGH EXPETORANTS",@"MUCOLYTICS",@"MIXED COUGH PREPARATIONS",@"NASAL",@"LOZENGES",@"ANTINIOTICS",@"ANTIEMETICS",@"ANTISPAMODIC",@"TOPICAL", nil];
@@ -163,7 +246,7 @@ NSArray *hed;
     [self.view addSubview:page1];
     [self.view bringSubviewToFront:page1];
 }
-
+*/
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -202,12 +285,12 @@ NSArray *hed;
     cell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;
     
     
-    if(page1.currentPage==0)
+    if(self.scrollerTable==tableView)
         [lblName setText:[labelArrayScroller objectAtIndex:indexPath.row]];
     else
         [lblName setText:[labelArrayScroller2 objectAtIndex:indexPath.row]];
     
-    
+    NSLog(@"lblName.text=%@",lblName.text);
     
     [lblName2 setText:@"Examples:Proingravida,nibh vel velit,aliquet"];
     [lblName setTextColor:[UIColor colorWithRed:49.0/255.0 green:191.0/255.0 blue:180.0/255.0 alpha:1.0]];
@@ -230,7 +313,7 @@ NSArray *hed;
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if(page1.currentPage==0)
+    if(self.scrollerTable==tableView)
         return labelArrayScroller.count;
     else
         return labelArrayScroller2.count;
