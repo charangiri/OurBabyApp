b9bb0dc10a8edb6def91b453c747b7a5f00cdd69
Arbaz <ta124@andreis-mac-mini.local>
2016-04-06 08:16:57 +0530

    Final commmit

--- BabyApp/BabyApp/BirthRecordTableViewController.m
+++ BabyApp/BabyApp/BirthRecordTableViewController.m
@@ -29,13 +29,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.minDurationView.layer.borderColor = [UIColor whiteColor].CGColor;
-    self.minDurationView.layer.borderWidth = 1.0f;
-    
-    
     self.maxDurationView.layer.borderColor = [UIColor whiteColor].CGColor;
     self.maxDurationView.layer.borderWidth = 1.0f;
     
+    self.minDurationView.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.minDurationView.layer.borderWidth = 1.0f;
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
